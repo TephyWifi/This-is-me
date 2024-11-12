a069f1863e0d1cdc52fd1f4d798a6c6f4c5e96ad
Stephanie <tephyreiso@proton.me>
2024-11-12 21:48:16 +0100

    Cleaned up and changed things..dont remember what

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
+// Navbar
+document.getElementById("overlay").addEventListener('click', closeNav);
+
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     var button = document.getElementById("myMenu");
     button.classList.add('hidden') 
+    document.getElementById("overlay").style.display = "block";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     var button = document.getElementById("myMenu");
     button.classList.remove ('hidden') 
+    document.getElementById("overlay").style.display = "none";
 }
 
-// function switchMode() {
-//     document.getElementById("dark-light");
-//     let element = document.body;
-//     element.classList.toggle("switchmode");
-// }
+// Switch between dark and light mode
+function switchMode() {
+    document.getElementById("modeLight");
+    let element = document.body;
+    element.classList.toggle("switchmode");
+}
 
+// Home icon function
 function goHome() {
     document.location.href="/index.html";
 }
